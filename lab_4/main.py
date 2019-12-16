06dfcfe4d0e0d99ba339c655a51b87b3c960ee7b
AnnaM0lkova <anna_small_devil@mail.ru>
2019-12-16 20:09:21 +0400

    Add token_func

--- lab_4/main.py
+++ lab_4/main.py
@@ -5,11 +5,45 @@ REFERENCE_TEXTS = []
 
 
 def clean_tokenize_corpus(texts: list) -> list:
+    if not texts or not isinstance(texts, list):
+        return []
+    clean_corpus = []
+    signs = [',', '-', '—', '«', '»', ':', ';', '"', "'", "#", "$", '(', ')', '&']
+    for one_text in texts:
+        if one_text and isinstance(one_text, str):
+            one_text = one_text.replace('?',".")
+            one_text = one_text.replace('!',".")
+            for i in signs:
+                one_text = one_text.replace(i," ")
+            while "  " in one_text:
+                one_text = one_text.replace("  "," ")
+            while '<\n>' in one_text:
+                one_text = one_text.replace("<\n/>", " ")
+            while '<br />' in one_text:
+                one_text = one_text.replace("<br />", " ")
+            clean_token_text = []
+            words = one_text.split(" ")
+            for word in words:
+                new_word = ""
+                if not word.isalpha():
+                    for i in word.lower():
+                        if i.isalpha():
+                            new_word += i
+                    if new_word:
+                        clean_token_text.append(new_word.lower())
+                else:
+                    clean_token_text.append(word.lower())
+            clean_corpus += [clean_token_text]
+    return clean_corpus
     pass
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
         pass
 
     def calculate_tf(self):
@@ -32,9 +66,13 @@ if __name__ == '__main__':
             REFERENCE_TEXTS.append(f.read())
     # scenario to check your work
     test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
+    print(test_texts)
+    '''
+    test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
     tf_idf = TfIdfCalculator(test_texts)
     tf_idf.calculate_tf()
     tf_idf.calculate_idf()
     tf_idf.calculate()
     print(tf_idf.report_on('good', 0))
     print(tf_idf.report_on('and', 1))
+    '''
